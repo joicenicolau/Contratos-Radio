ed7ce2027e59056eddae644054a1e6b2a1e03880
joicenicolau <joice_nicolau@hotmail.com>
2024-07-27 17:24:55 -0300

    Dashboard

--- radio/src/App.js
+++ radio/src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Login from './components/Login';
-// import Dashboard from './components/Dashboard';
+import Dashboard from './components/Dashboard';
 // import Contracts from './components/Contracts';
 // import CreateContract from './components/CreateContract';
 
@@ -9,9 +9,9 @@ function App() {
   return (
     <div className="App">
       <Routes>
-        <Route path="/" element={<Login />} />
-        {/* <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/contracts" element={<Contracts />} />
+        <Route exact path="/" element={<Login />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        {/* <Route path="/contracts" element={<Contracts />} />
         <Route path="/create-contract" element={<CreateContract />} /> */}
       </Routes>
     </div>
---
+++ radio/src/components/Dashboard.jsx
@@ -0,0 +1,61 @@
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import { Pie } from 'react-chartjs-2';
+import {
+  Chart as ChartJS,
+  ArcElement,
+  Tooltip,
+  Legend
+} from 'chart.js';
+import './Dashboard.css';
+
+ChartJS.register(ArcElement, Tooltip, Legend);
+
+function Dashboard() {
+  const [contracts, setContracts] = useState([]);
+
+  useEffect(() => {
+    const fetchContracts = async () => {
+      try {
+        const response = await axios.get('https://teste-front-1.azurewebsites.net/contracts');
+        console.log('Dados dos contratos:', response.data);  // erro dos dados, retirar depois
+        setContracts(response.data);
+      } catch (error) {
+        console.error("Erro ao buscar contratos:", error);
+      }
+    };
+    fetchContracts();
+  }, []);
+
+  const contractStatusData = {
+    labels: ['Ativo', 'Expirado', 'Pendente'],
+    datasets: [
+      {
+        label: 'Status dos Contratos',
+        data: [
+          contracts.filter(contract => contract.status === 'ativo').length,
+          contracts.filter(contract => contract.status === 'expirado').length,
+          contracts.filter(contract => contract.status === 'pendente').length,
+        ],
+        backgroundColor: ['#4caf50', '#f44336', '#ff9800'],
+      },
+    ],
+  };
+
+  console.log('Dados para o gráfico:', contractStatusData);  // Log dos gráfico
+
+  return (
+    <div className="dashboard-container">
+      <h2>Dashboard</h2>
+      <div className="chart-container">
+        {contracts.length > 0 ? (
+          <Pie data={contractStatusData} />
+        ) : (
+          <p>Carregando dados...</p>
+        )}
+      </div>
+    </div>
+  );
+}
+
+export default Dashboard;
